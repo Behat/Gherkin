21e1d5128fc975f9022c62d358a49d675587ae32
ever.zet <ever.zet@gmail.com>
2011-02-13 12:05:12 +0200

    refactored ParserTest

--- tests/Behat/Gherkin/ParserTest.php
+++ tests/Behat/Gherkin/ParserTest.php
@@ -14,62 +14,84 @@ class ParserTest extends \PHPUnit_Framework_TestCase
     private $gherkin;
     private $yaml;
 
-    protected function setUp()
+    protected function getGherkinParser()
     {
-        $keywords       = new ArrayKeywords(array(
-            'en' => array(
-                'Feature'           => 'Feature',
-                'Background'        => 'Background',
-                'Scenario'          => 'Scenario',
-                'Scenario Outline'  => 'Scenario Outline',
-                'Examples'          => 'Examples',
-                'Step Types'        => 'Given|When|Then|And|But'
-            ),
-            'ru' => array(
-                'Feature'           => 'Функционал',
-                'Background'        => 'Предыстория',
-                'Scenario'          => 'Сценарий',
-                'Scenario Outline'  => 'Структура сценария',
-                'Examples'          => 'Значения',
-                'Step Types'        => 'Допустим|То|Если|И|Но'
-            )
-        ));
-        $this->gherkin  = new Parser(new Lexer($keywords));
-        $this->yaml     = new YamlFileLoader();
+        if (null === $this->gherkin) {
+            $keywords       = new ArrayKeywords(array(
+                'en' => array(
+                    'Feature'           => 'Feature',
+                    'Background'        => 'Background',
+                    'Scenario'          => 'Scenario',
+                    'Scenario Outline'  => 'Scenario Outline',
+                    'Examples'          => 'Examples',
+                    'Step Types'        => 'Given|When|Then|And|But'
+                ),
+                'ru' => array(
+                    'Feature'           => 'Функционал',
+                    'Background'        => 'Предыстория',
+                    'Scenario'          => 'Сценарий',
+                    'Scenario Outline'  => 'Структура сценария',
+                    'Examples'          => 'Значения',
+                    'Step Types'        => 'Допустим|То|Если|И|Но'
+                )
+            ));
+            $this->gherkin  = new Parser(new Lexer($keywords));
+        }
+
+        return $this->gherkin;
+    }
+
+    protected function getYamlParser()
+    {
+        if (null === $this->yaml) {
+            $this->yaml = new YamlFileLoader();
+        }
+
+        return $this->yaml;
     }
 
     protected function parseFixture($fixture)
     {
-        return $this->gherkin->parse(__DIR__ . '/Fixtures/features/' . $fixture);
+        return $this->getGherkinParser()->parse(__DIR__ . '/Fixtures/features/' . $fixture);
     }
 
     protected function parseEtalon($etalon)
     {
-        $features = $this->yaml->load(__DIR__ . '/Fixtures/etalons/' . $etalon);
+        $features = $this->getYamlParser()->load(__DIR__ . '/Fixtures/etalons/' . $etalon);
         $feature  = $features[0];
         $feature->setFile(__DIR__ . '/Fixtures/features/' . basename($etalon, '.yml') . '.feature');
 
         return $feature;
     }
 
-    public function testFixtures()
+    public function parserTestDataProvider()
     {
+        $data = array();
+
         $finder = new Finder();
         $files  = $finder->files()->name('*.yml')->in(__DIR__ . '/Fixtures/etalons');
 
         foreach ($files as $file) {
             $testname = basename($file, '.yml');
 
-            $etalonFeature = $this->parseEtalon($testname . '.yml');
+            $etalonFeature      = $this->parseEtalon($testname . '.yml');
+            $fixtureFeatures    = $this->parseFixture($testname . '.feature');
 
-            $features = $this->parseFixture($testname . '.feature');
-            $this->assertType('array', $features);
-            $this->assertEquals(1, count($features));
-            $fixtureFeature = $features[0];
-
-            $this->assertEquals(
-                $etalonFeature, $fixtureFeature, sprintf('Testing "%s"', $testname)
-            );
+            $data[] = array($testname, $etalonFeature, $fixtureFeatures);
         }
+
+        return $data;
+    }
+
+    /**
+     * @dataProvider parserTestDataProvider
+     */
+    public function testFixtures($fixtureName, $etalon, $features)
+    {
+        $this->assertType('array', $features);
+        $this->assertEquals(1, count($features));
+        $fixture = $features[0];
+
+        $this->assertEquals($etalon, $fixture);
     }
 }
