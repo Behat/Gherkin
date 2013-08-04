8467c88281f75b9368b1c7542abd4f5510b6a151
everzet <ever.zet@gmail.com>
2013-08-04 01:30:30 +0100

    Add in-memory cache for features
    To avoid loading same features twice in one run

---
+++ tests/Behat/Gherkin/Cache/MemoryCacheTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Behat\Gherkin\Cache;
+
+use Behat\Gherkin\Cache\MemoryCache,
+    Behat\Gherkin\Node\ScenarioNode,
+    Behat\Gherkin\Node\FeatureNode;
+
+class MemoryCacheTest extends \PHPUnit_Framework_TestCase
+{
+    private $cache;
+
+    protected function setUp()
+    {
+        $this->cache = new MemoryCache();
+    }
+
+    public function testIsFreshWhenThereIsNoFile()
+    {
+        $this->assertFalse($this->cache->isFresh('unexisting', time() + 100));
+    }
+
+    public function testIsFreshOnFreshFile()
+    {
+        $feature = new FeatureNode();
+
+        $this->cache->write('some_path', $feature);
+
+        $this->assertFalse($this->cache->isFresh('some_path', time() + 100));
+    }
+
+    public function testIsFreshOnOutdated()
+    {
+        $feature = new FeatureNode();
+
+        $this->cache->write('some_path', $feature);
+
+        $this->assertTrue($this->cache->isFresh('some_path', time() - 100));
+    }
+
+    public function testCacheAndRead()
+    {
+        $feature = new FeatureNode('Some feature', 'some description');
+        $feature->addScenario(new ScenarioNode('Some scenario'));
+
+        $this->cache->write('some_feature', $feature);
+        $featureRead = $this->cache->read('some_feature');
+
+        $this->assertEquals($feature, $featureRead);
+    }
+}
