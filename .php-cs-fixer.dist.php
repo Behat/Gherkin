b1122ec53318719caca2f1d8bca74d867b95b583
Christian Sciberras <christian@sciberras.me>
2024-12-07 19:20:35 +0100

    Set up PHP-CS-Fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,11 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__);
+
+return (new PhpCsFixer\Config())
+    ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
+    ->setRules([
+        '@PER-CS' => true,
+    ])
+    ->setFinder($finder);
