50fdacc4a8df5053314ea1f6460d398f7485a094
Christian Sciberras <christian@sciberras.me>
2024-12-11 23:50:31 +0100

    Setup PHP-CS-Fixer config and pipeline

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,29 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->notPath('i18n.php');
+
+return (new PhpCsFixer\Config())
+    ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
+    ->setRules([
+        '@PER-CS' => true,
+        '@Symfony' => true,
+        'header_comment' => [
+            'header' => <<<'TEXT'
+                This file is part of the Behat Gherkin Parser.
+                (c) Konstantin Kudryashov <ever.zet@gmail.com>
+                
+                For the full copyright and license information, please view the LICENSE
+                file that was distributed with this source code.
+                TEXT
+        ],
+        'yoda_style' => [
+            'equal' => false,
+            'identical' => false,
+            'less_and_greater' => false,
+        ],
+        'concat_space' => ['spacing' => 'one'],
+        'phpdoc_align' => ['align' => 'left'],
+    ])
+    ->setFinder($finder);
