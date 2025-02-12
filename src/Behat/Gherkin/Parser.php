cfd20b4689cf2dd1680d4b00958896882612878d
Christian Sciberras <uuf6429@gmail.com>
2025-02-12 16:09:20 +0100

    Normalise/simplify phpstan type

--- src/Behat/Gherkin/Parser.php
+++ src/Behat/Gherkin/Parser.php
@@ -32,7 +32,7 @@ use Behat\Gherkin\Node\TableNode;
  *
  * @author Konstantin Kudryashov <ever.zet@gmail.com>
  *
- * @phpstan-type TParsedExpressionResult FeatureNode|BackgroundNode|ScenarioNode|OutlineNode|ExampleTableNode|TableNode|PyStringNode|StepNode|string|"\n"|''
+ * @phpstan-type TParsedExpressionResult FeatureNode|BackgroundNode|ScenarioNode|OutlineNode|ExampleTableNode|TableNode|PyStringNode|StepNode|string
  */
 class Parser
 {
