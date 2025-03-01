70cb0f312238a88ed31820103a0a9fa3ae2e3937
Christian Sciberras <christian@sciberras.me>
2025-03-01 12:15:09 +0100

    Add general test

--- tests/Loader/CucumberNDJsonAstLoaderTest.php
+++ tests/Loader/CucumberNDJsonAstLoaderTest.php
@@ -12,6 +12,11 @@ namespace Tests\Behat\Gherkin\Loader;
 
 use Behat\Gherkin\Exception\NodeException;
 use Behat\Gherkin\Loader\CucumberNDJsonAstLoader;
+use Behat\Gherkin\Node\BackgroundNode;
+use Behat\Gherkin\Node\ExampleTableNode;
+use Behat\Gherkin\Node\FeatureNode;
+use Behat\Gherkin\Node\OutlineNode;
+use Behat\Gherkin\Node\ScenarioNode;
 use PHPUnit\Framework\TestCase;
 
 final class CucumberNDJsonAstLoaderTest extends TestCase
@@ -26,6 +31,99 @@ final class CucumberNDJsonAstLoaderTest extends TestCase
     public function testStringResourcesAreSupported(): void
     {
         $this->assertTrue($this->loader->supports('a string'));
+    }
+
+    public function testValidLoading(): void
+    {
+        $file = $this->serializeCucumberMessagesToFile([
+            'gherkinDocument' => [
+                'feature' => [
+                    'location' => ['line' => 111],
+                    'description' => 'Feature with a valid Scenario',
+                    'keyword' => 'fea',
+                    'language' => 'en',
+                    'children' => [
+                        [
+                            'background' => [
+                                'location' => ['line' => 222],
+                                'keyword' => 'bac',
+                                'name' => 'Empty Background',
+                            ],
+                        ],
+                        [
+                            'scenario' => [
+                                'location' => ['line' => 333],
+                                'keyword' => 'sce',
+                                'name' => 'Empty Scenario',
+                                'examples' => [],
+                            ],
+                        ],
+                        [
+                            'scenario' => [
+                                'location' => ['line' => 444],
+                                'keyword' => 'out',
+                                'examples' => [
+                                    [
+                                        'location' => ['line' => 555],
+                                        'keyword' => 'exa',
+                                        'tableHeader' => [
+                                            'location' => ['line' => 666],
+                                            'cells' => [
+                                                ['value' => 'A'],
+                                                ['value' => 'B'],
+                                            ],
+                                        ],
+                                        'tableBody' => [
+                                            [
+                                                'location' => ['line' => 777],
+                                                'cells' => [
+                                                    ['value' => 'A1'],
+                                                    ['value' => 'B1'],
+                                                ],
+                                            ],
+                                            [
+                                                'location' => ['line' => 888],
+                                                'cells' => [
+                                                    ['value' => 'A2'],
+                                                    ['value' => 'B2'],
+                                                ],
+                                            ],
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ]);
+        $features = $this->loader->load($file);
+
+        $this->assertEquals(
+            [
+                new FeatureNode(
+                    null,
+                    'Feature with a valid Scenario',
+                    [],
+                    new BackgroundNode('Empty Background', [], 'bac', 222),
+                    [
+                        new ScenarioNode('Empty Scenario', [], [], 'sce', 333),
+                        new OutlineNode(null, [], [], [
+                            new ExampleTableNode([
+                                666 => ['A', 'B'],
+                                777 => ['A1', 'B1'],
+                                888 => ['A2', 'B2'],
+                            ], 'exa'),
+                        ], 'out', 444),
+                    ],
+                    'fea',
+                    'en',
+                    $file,
+                    111,
+                ),
+            ],
+            $features,
+        );
     }
 
     public function testOutlineTableHeaderIsRequired(): void
