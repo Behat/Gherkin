442391e808503d75e3307acd474fa0ffaef5d7fb
acoulton <andrew@ingenerator.com>
2025-04-24 00:39:04 +0100

    Rewrite GitHub action and script for updating Gherkin dev dependency
    This has been broken since cucumber reorganised their repositories. As
    well as moving to a new repository, the tagging and changelog structure
    is different. There were also some issues with the old scripts - e.g.
    the changelog was not properly escaped in the PR body (see #255 for an
    example of broken content & links).
    
    I have rewritten the flow so that:
    
    * We use the GitHub API to fetch release details and changelogs, rather
      than trying to parse the changelog markdown file.
    * We only bump one Gherkin release per workflow run (so that we can see
      clearly which release causes tests to fail, if any).
    * We include the Gherkin release details in the commit message (the old
      script could bump multiple releases in a single commit but only showed
      the most recent changelog in the PR body, which was misleading).
    * If there is already a PR open, we append subsequent releases to the
      same PR.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,7 +2,7 @@
 
 $finder = (new PhpCsFixer\Finder())
     ->in(__DIR__)
-    ->name(['cucumber_changelog', 'update_cucumber', 'update_i18n'])
+    ->name(['update_cucumber', 'update_i18n'])
     ->notPath('i18n.php');
 
 return (new PhpCsFixer\Config())
@@ -14,7 +14,7 @@ return (new PhpCsFixer\Config())
             'header' => <<<'TEXT'
                 This file is part of the Behat Gherkin Parser.
                 (c) Konstantin Kudryashov <ever.zet@gmail.com>
-                
+
                 For the full copyright and license information, please view the LICENSE
                 file that was distributed with this source code.
                 TEXT
