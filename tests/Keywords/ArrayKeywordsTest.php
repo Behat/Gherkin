1bf4dcfe49001338b720aabebaa0efdaf490f856
Christophe Coevoet <stof@notk.org>
2025-05-23 12:48:38 +0200

    Ensure a valid instantiation of the ArrayKeywords in tests
    ArrayKeywords makes its language default to `en` and assumes that there
    is data available for it without checking. When this is not the case,
    calling its methods before setting another language will trigger a PHP
    warning and return `null` (in methods where the phpdoc says it must
    return a string).
    The ArrayKeywords test is currently passing only because the current way
    the parser works avoids this broken state for the particular case being
    used in the test, but this is not guaranteed to stay true.

--- tests/Keywords/ArrayKeywordsTest.php
+++ tests/Keywords/ArrayKeywordsTest.php
@@ -18,7 +18,24 @@ class ArrayKeywordsTest extends KeywordsTestCase
 {
     protected static function getKeywords(): KeywordsInterface
     {
-        return new ArrayKeywords(static::getKeywordsArray());
+        return new ArrayKeywords(static::getKeywordsArray() + [
+            // ArrayKeywords assumes internally that `en` is always a supported language
+            'en' => [
+                'and' => 'And|*',
+                'background' => 'Background',
+                'but' => 'But|*',
+                'examples' => 'Scenarios|Examples',
+                'feature' => 'Business Need|Ability|Feature',
+                'given' => 'Given|*',
+                'name' => 'English',
+                'native' => 'English',
+                'rule' => 'Rule',
+                'scenario' => 'Scenario|Example',
+                'scenario_outline' => 'Scenario Template|Scenario Outline',
+                'then' => 'Then|*',
+                'when' => 'When|*',
+            ],
+        ]);
     }
 
     protected static function getKeywordsArray(): array
