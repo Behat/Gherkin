c0bab297d90c90ba51e7432c2112fda92308c0a0
Christophe Coevoet <stof@notk.org>
2025-05-26 15:47:26 +0200

    Increase test coverage for the CucumberNDJsonAstLoader

--- tests/Loader/CucumberNDJsonAstLoaderTest.php
+++ tests/Loader/CucumberNDJsonAstLoaderTest.php
@@ -16,7 +16,10 @@ use Behat\Gherkin\Node\BackgroundNode;
 use Behat\Gherkin\Node\ExampleTableNode;
 use Behat\Gherkin\Node\FeatureNode;
 use Behat\Gherkin\Node\OutlineNode;
+use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\ScenarioNode;
+use Behat\Gherkin\Node\StepNode;
+use Behat\Gherkin\Node\TableNode;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -76,7 +79,40 @@ final class CucumberNDJsonAstLoaderTest extends TestCase
                                 'name' => 'Examples Scenario',
                                 'description' => '',
                                 'tags' => [],
-                                'steps' => [],
+                                'steps' => [
+                                    [
+                                        'location' => ['line' => 500],
+                                        'keyword' => 'Given ',
+                                        'keywordType' => 'Context',
+                                        'text' => 'some step',
+                                        'docString' => [
+                                            'location' => ['line' => 501],
+                                            'delimiter' => '"""',
+                                            'content' => 'some text',
+                                        ],
+                                        'id' => '',
+                                    ],
+                                    [
+                                        'location' => ['line' => 510],
+                                        'keyword' => 'When ',
+                                        'keywordType' => 'Action',
+                                        'text' => 'some other step',
+                                        'dataTable' => [
+                                            'location' => ['line' => 511],
+                                            'rows' => [
+                                                [
+                                                    'location' => ['line' => 511],
+                                                    'cells' => [
+                                                        ['location' => ['line' => 511], 'value' => 'A'],
+                                                        ['location' => ['line' => 511], 'value' => 'B'],
+                                                    ],
+                                                    'id' => '',
+                                                ],
+                                            ],
+                                        ],
+                                        'id' => '',
+                                    ],
+                                ],
                                 'id' => '',
                                 'keyword' => 'out',
                                 'examples' => [
@@ -134,7 +170,10 @@ final class CucumberNDJsonAstLoaderTest extends TestCase
                     new BackgroundNode('Empty Background', [], 'bac', 222),
                     [
                         new ScenarioNode('Empty Scenario', [], [], 'sce', 333),
-                        new OutlineNode('Examples Scenario', [], [], [
+                        new OutlineNode('Examples Scenario', [], [
+                            new StepNode('Given', 'some step', [new PyStringNode(['some text'], 501)], 500, 'Given'),
+                            new StepNode('When', 'some other step', [new TableNode([511 => ['A', 'B']])], 510, 'When'),
+                        ], [
                             new ExampleTableNode([
                                 666 => ['A', 'B'],
                                 777 => ['A1', 'B1'],
@@ -281,6 +320,20 @@ final class CucumberNDJsonAstLoaderTest extends TestCase
             ],
             $features,
         );
+    }
+
+    public function testLoadingEmptyDocument(): void
+    {
+        $file = $this->serializeCucumberMessagesToFile([
+            'gherkinDocument' => [
+                'comments' => [],
+                'uri' => '../testdata/good/empty.feature',
+            ],
+        ]);
+
+        $features = $this->loader->load($file);
+
+        $this->assertCount(0, $features);
     }
 
     /**
