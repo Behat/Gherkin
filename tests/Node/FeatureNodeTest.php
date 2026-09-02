e048bc2f6d8e4f10a1c9bbaca85180294e35ba12
acoulton <andrew@ingenerator.com>
2026-09-02 17:55:48 +0100

    feat: FeatureNode::getScenarios() should emit all scenarios with rules
    To make the new Rule parsing backwards-compatible with existing test
    runners, getScenarios must return a flat list of `ScenarioInterface[]`.
    
    We achieve this by hoisting any Scenario / Outline from inside a Rule as
    though it had been written as part of the top-level Feature.
    
    This also means collapsing any Rule Background steps into the start of
    each Scenario, rather than exposing them as a separate list of steps.
    
    Existing test runners will not see the detail of the Rule groupings, or
    the division between the Background and Scenario parts of the Rule (the
    Feature background is still separate, for BC).
    
    In many cases, this has no impact - this just takes us in the same
    direction as Cucumber, where the `pickles` passed to the test runner
    contain the combined list of steps to run for each test case.
    
    It will only really be relevant for output formatters, particularly ones
    that attempt to render a gherkin-style output. These will render without
    breaking, but the grouping of Scenarios and Steps will not match the
    gherkin source.
    
    Formatters can resolve this by upgrading to call `getChildren` and
    matching nodes by e.g. line number in the source. Again, this is similar
    to how official Gherkin formatters have to match the execution results
    from `pickles` back to the (separate) `AST` from the parser.

---
+++ tests/Node/FeatureNodeTest.php
@@ -0,0 +1,245 @@
+<?php
+
+/*
+ * This file is part of the Behat Gherkin Parser.
+ * (c) Konstantin Kudryashov <ever.zet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\Behat\Gherkin\Node;
+
+use Behat\Gherkin\Node\BackgroundNode;
+use Behat\Gherkin\Node\FeatureNode;
+use Behat\Gherkin\Node\RuleNode;
+use Behat\Gherkin\Node\ScenarioInterface;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+use UnexpectedValueException;
+
+/**
+ * @phpstan-type TFeatureChild BackgroundNode|RuleNode|ScenarioInterface
+ */
+class FeatureNodeTest extends TestCase
+{
+    /**
+     * @phpstan-return iterable<string, list{FeatureNode, list<TFeatureChild>}>
+     */
+    public static function providerSupportedChildren(): iterable
+    {
+        $background = StubNode::background(title: 'Background');
+        $scenario1 = StubNode::scenario(title: 'Scenario 1');
+        $rule1 = StubNode::rule(title: 'Rule');
+
+        yield 'no children' => [
+            StubNode::feature(background: null, scenarios: []),
+            [],
+        ];
+
+        yield 'background only' => [
+            StubNode::feature(background: $background, scenarios: []),
+            [$background],
+        ];
+
+        yield 'background and scenarios' => [
+            StubNode::feature(background: $background, scenarios: [$scenario1]),
+            [$background, $scenario1],
+        ];
+
+        yield 'background and scenarios and rules' => [
+            StubNode::feature(background: $background, scenarios: [$scenario1, $rule1]),
+            [$background, $scenario1, $rule1],
+        ];
+
+        yield 'rules with no background' => [
+            StubNode::feature(background: null, scenarios: [$rule1]),
+            [$rule1],
+        ];
+    }
+
+    /**
+     * @phpstan-param list<TFeatureChild> $expect
+     */
+    #[DataProvider('providerSupportedChildren')]
+    public function testGetChildrenReturnsChildrenOfAllTypes(FeatureNode $feature, array $expect): void
+    {
+        $this->assertSame($expect, $feature->getChildren());
+    }
+
+    /**
+     * @phpstan-return iterable<string, list{FeatureNode, list<ScenarioInterface>}>
+     */
+    public static function providerRulesBackwardsCompatibility(): iterable
+    {
+        yield 'empty list for empty feature' => [
+            StubNode::feature(background: null, scenarios: []),
+            [],
+        ];
+
+        yield 'empty list when only a background' => [
+            StubNode::feature(
+                background: StubNode::background(),
+                scenarios: [],
+            ),
+            [],
+        ];
+
+        $scenario1 = StubNode::scenario(title: 'Scenario 1');
+        $outline1 = StubNode::outline(title: 'Outline 1');
+
+        yield 'original scenarios and outlines when no rules' => [
+            StubNode::feature(
+                background: StubNode::background(),
+                scenarios: [$scenario1, $outline1],
+            ),
+            [$scenario1, $outline1],
+        ];
+
+        yield 'empty when only empty rules' => [
+            StubNode::feature(
+                background: null,
+                scenarios: [StubNode::rule()],
+            ),
+            [],
+        ];
+
+        yield 'un-nests scenarios from inside single rule' => [
+            StubNode::feature(
+                background: null,
+                scenarios: [
+                    StubNode::rule(children: [$scenario1, $outline1]),
+                ],
+            ),
+            [$scenario1, $outline1],
+        ];
+
+        $scenario2 = StubNode::scenario(title: 'Scenario 2');
+        $scenario3 = StubNode::scenario(title: 'Scenario 3');
+
+        yield 'un-nests in order with mix of scenarios and rules' => [
+            StubNode::feature(
+                background: null,
+                scenarios: [
+                    $scenario2,
+                    StubNode::rule(children: [$scenario1, $outline1]),
+                    StubNode::rule(children: [$scenario3]),
+                ],
+            ),
+            [$scenario2, $scenario1, $outline1, $scenario3],
+        ];
+
+        yield 'merges rule background steps into each scenario' => [
+            StubNode::feature(
+                background: StubNode::background(steps: [
+                    StubNode::step(keyword: 'Given', text: 'background state for all scenarios'),
+                ]),
+                scenarios: [
+                    StubNode::rule(children: [
+                        StubNode::background(steps: [
+                            StubNode::step(keyword: 'Given', text: 'some pre-existing state'),
+                            StubNode::step(keyword: 'And', text: 'some other precondition'),
+                        ]),
+                        StubNode::scenario(
+                            title: 'Scenario with steps',
+                            tags: ['slow'],
+                            steps: [
+                                StubNode::step('When', 'I do something'),
+                            ],
+                            keyword: 'Story',
+                            line: 14,
+                            description: 'This scenario has steps',
+                        ),
+                        StubNode::outline(
+                            title: 'Outline with steps',
+                            tags: ['fast'],
+                            steps: [
+                                StubNode::step('When', 'They do something'),
+                            ],
+                            tables: [StubNode::exampleTable(table: [['one']])],
+                            keyword: 'Scenario',
+                            line: 15,
+                            description: 'Some outline with info',
+                        ),
+                        StubNode::scenario(
+                            title: 'Scenario with no steps',
+                            steps: [],
+                        ),
+                    ]),
+                ],
+            ),
+            [
+                StubNode::scenario(
+                    title: 'Scenario with steps',
+                    tags: ['slow'],
+                    steps: [
+                        StubNode::step(keyword: 'Given', text: 'some pre-existing state'),
+                        StubNode::step(keyword: 'And', text: 'some other precondition'),
+                        StubNode::step('When', 'I do something'),
+                    ],
+                    keyword: 'Story',
+                    line: 14,
+                    description: 'This scenario has steps',
+                ),
+                StubNode::outline(
+                    title: 'Outline with steps',
+                    tags: ['fast'],
+                    steps: [
+                        StubNode::step(keyword: 'Given', text: 'some pre-existing state'),
+                        StubNode::step(keyword: 'And', text: 'some other precondition'),
+                        StubNode::step('When', 'They do something'),
+                    ],
+                    tables: [StubNode::exampleTable(table: [['one']])],
+                    keyword: 'Scenario',
+                    line: 15,
+                    description: 'Some outline with info',
+                ),
+                StubNode::scenario(
+                    title: 'Scenario with no steps',
+                    steps: [
+                        StubNode::step(keyword: 'Given', text: 'some pre-existing state'),
+                        StubNode::step(keyword: 'And', text: 'some other precondition'),
+                    ],
+                ),
+            ],
+        ];
+    }
+
+    /**
+     * @phpstan-param list<ScenarioInterface> $expect
+     */
+    #[DataProvider('providerRulesBackwardsCompatibility')]
+    public function testGetScenariosExpandsRulesToScenariosForBackwardsCompatibility(FeatureNode $feature, array $expect): void
+    {
+        $scenarios = $feature->getScenarios();
+        foreach ($scenarios as $scenario) {
+            $this->assertInstanceOf(ScenarioInterface::class, $scenario);
+        }
+
+        $this->assertEquals($expect, $feature->getScenarios());
+    }
+
+    public function testGetScenariosThrowsOnAttemptToMergeRuleWithCustomScenarioClass(): void
+    {
+        $feature = StubNode::feature(
+            scenarios: [
+                StubNode::rule(
+                    children: [
+                        StubNode::background(
+                            steps: [
+                                StubNode::step(keyword: 'Given', text: 'some pre-existing state'),
+                            ]
+                        ),
+                        // Per our phpdoc, it is valid for a Rule to take any ScenarioInterface - a user could
+                        // theoretically extend the Parser to build custom objects instead of ScenarioNode.
+                        $this->getMockBuilder(ScenarioInterface::class)->getMock(),
+                    ]
+                ),
+            ]
+        );
+
+        $this->expectException(UnexpectedValueException::class);
+        $this->expectExceptionMessage('Rule children must be instances of ScenarioNode');
+        $feature->getScenarios();
+    }
+}
