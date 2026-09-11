3f28d2529378bfcfd98cb53fa1ffb9c9c9da7944
acoulton <andrew@ingenerator.com>
2026-09-11 21:19:21 +0100

    refactor: Refactor LineRangeFilter internals
    Use a consistent helper method for checking if the line is in range, to
    show that this check is always the same.

--- src/Filter/LineRangeFilter.php
+++ src/Filter/LineRangeFilter.php
@@ -51,8 +51,7 @@ class LineRangeFilter implements FilterInterface
      */
     public function isFeatureMatch(FeatureNode $feature)
     {
-        return $this->filterMinLine <= $feature->getLine()
-            && $this->filterMaxLine >= $feature->getLine();
+        return $this->isInLineRange($feature->getLine());
     }
 
     /**
@@ -66,14 +65,14 @@ class LineRangeFilter implements FilterInterface
      */
     public function isScenarioMatch(ScenarioInterface $scenario)
     {
-        if ($this->filterMinLine <= $scenario->getLine() && $this->filterMaxLine >= $scenario->getLine()) {
+        if ($this->isInLineRange($scenario->getLine())) {
             return true;
         }
 
         if ($scenario instanceof OutlineNode && $scenario->hasExamples()) {
             foreach ($scenario->getExampleTables() as $table) {
                 foreach ($table->getLines() as $line) {
-                    if ($this->filterMinLine <= $line && $this->filterMaxLine >= $line) {
+                    if ($this->isInLineRange($line)) {
                         return true;
                     }
                 }
@@ -108,7 +107,7 @@ class LineRangeFilter implements FilterInterface
                     unset($table[$lines[0]]);
 
                     foreach ($table as $line => $row) {
-                        if ($this->filterMinLine <= $line && $this->filterMaxLine >= $line) {
+                        if ($this->isInLineRange($line)) {
                             $filteredTable[$line] = $row;
                         }
                     }
@@ -125,5 +124,10 @@ class LineRangeFilter implements FilterInterface
         }
 
         return $feature->withScenarios($scenarios);
+    }
+
+    private function isInLineRange(int $line): bool
+    {
+        return $this->filterMinLine <= $line && $this->filterMaxLine >= $line;
     }
 }
